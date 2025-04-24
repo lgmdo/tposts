f7993fdd76032aabfa9674c4f360f1e3fe9ef990
Leonardo Moreira <70420142+lgmdo@users.noreply.github.com>
2025-04-23 22:10:07 -0300

    Revert "feat: add sign up"

--- entrypoint.dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-poetry install
-poetry run python src/manage.py makemigrations
-poetry run python src/manage.py migrate
-poetry run python src/manage.py runserver 0.0.0.0:8000
-
--- src/apps/health/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-from .views import HealthCheckView
-
-urlpatterns = [path("health", HealthCheckView.as_view(), name="health-check")]
